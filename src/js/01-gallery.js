aab2041eb20e1b4fa8f6395f9eec9c80856157d2
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-02-20 12:08:59 +0300

    Зміни до восьмого ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const items = [];
+
+galleryItems.map((element) => {
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = element.original;
+
+    const galleryImg = document.createElement('img');
+    galleryImg.classList.add('gallery__image');
+    galleryImg.src = element.preview;
+    galleryImg.setAttribute('title', element.description);
+    galleryImg.alt = element.description;
+
+    galleryLink.append(galleryImg);
+    items.push(galleryLink);
+});
+
+gallery.append(...items);
+
+new SimpleLightbox('.gallery a', {
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
