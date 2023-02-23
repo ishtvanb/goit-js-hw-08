62a337ca73fe4066102cce15fb8233e4a43e4c8f
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-02-23 16:23:01 +0200

    Зміни до восьмого ДЗ

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const STORAGE_KEY = 'feedback-form-state';
+
+const objectToSave = { email: email.value, message: message.value };
+
+form.addEventListener('input', throttle(e => {
+    
+localStorage.setItem(STORAGE_KEY, JSON.stringify(objectToSave));
+}, 500));
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    if (email.value === '' || message.value === '') {
+        return alert('Заповни всі поля!');
+    }
+
+    console.log({ email: email.value, message: message.value });
+
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+});
+
+function fillInForm() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedData) {
+        objectToSave = savedData;
+    }
+}
+    
+
+     
+
+ 
