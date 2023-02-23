55530a5cd24cb4e4e2ec3ed89a300fa4a3f5a329
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-02-23 19:14:35 +0200

    Зміни до восьмого ДЗ

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,12 +2,12 @@ import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
-
 const player = new Player(iframe);
+const STORAGE_KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
+    localStorage.setItem(STORAGE_KEY, e.seconds);
 }, 1000)
 );
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0)
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0)
